07d6359a4eaff542d9c3c66bd0fdea6d78af0c89
Ditto <Zgamer96@gmail.com>
2017-11-22 19:54:48 -0500

    instance Krypto and being doDeal()

---
+++ KryptoApplication.java
@@ -0,0 +1,13 @@
+public class KryptoApplication
+{
+    public static void main(String[] args)
+    {
+        Krypto crypto = new Krypto();
+        //crypto.test();
+        
+        KryptoGUI gui = new KryptoGUI("CryptoDraft-GUI");
+    }
+}
+
+//import java.io.IOException;
+//throws IO Exception
